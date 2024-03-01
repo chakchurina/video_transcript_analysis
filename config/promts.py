3667da65386ffe5a42ce46a5473b33141dd7812e
chakchurina <maria.chakchurina@yandex.ru>
2024-02-29 22:57:28 -0300

    Add .gitkeeps

--- config/promts.py
+++ config/promts.py
@@ -3,7 +3,9 @@ Input:
 You have part of a podcast transcript with sentence numbers in a format <number: sentence>. 
 
 Task:
-Pick sentences from the input text, so that they tell a coherent story. 
+Select sentences from the input text, so that they tell a coherent story.
+
+Additionally:
 Think about placing sentence that contain {keywords} first. 
 Try to include sentence number {central} in the sequence, if it fits.
 Consider adding sentence that has conclusion, generalisation or prognosis, at the end of sequence.
@@ -20,4 +22,24 @@ Transcript:
 Restrictions:
 - output only comma-separated numbers,
 - there MUST be at least {smallest} sentences in response, but no mora than {largest}.
+"""
+
+verification_prompt_template = """
+Input:
+You have a sequence scripts for short videos in a format number:text. 
+
+Task:
+Choose from the provided fragments those that read as coherent and logical texts.
+
+Result: 
+Return comma-separated numbers of selected scripts.
+
+Example of the result: 
+1, 3, 4, 15, 21, 35
+
+Transcript:
+{transcript}
+
+Restrictions:
+- output only comma-separated numbers.
 """
